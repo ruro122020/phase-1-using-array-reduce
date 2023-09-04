8933a8be64fa14beef7aae44d0bdea8c13f7d35e
Ruth <ruro122020@gmail.com>
2023-09-04 17:46:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 0
+
+ batteryBatches.reduce((acc, element)=>{
+    return totalBatteries = acc + element
+},totalBatteries)
